5ac3b107a65680468af214ab701c77c037e4b7f1
igor.muller <igor.muller@SNFNS0ATICET107.sistemafiesc.org>
2017-04-19 12:53:05 -0300

    Incluído o Template AdminLTE

--- views/home.php
+++ views/home.php
@@ -1,1 +1,19 @@
-Home
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Home
+            <small>Página de controle do Conta Azul</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li class="active">Home</li>
+        </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        fgsdfgdfgsdg
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
