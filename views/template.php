816aa75675ad61bea8c7b9e863389ff0fec1dbd6
igor.muller <igor_muller@hotmail.com>
2017-05-08 16:28:17 -0300

    Ajustado informações do tamplate, para ativar menus.

--- views/template.php
+++ views/template.php
@@ -1,3 +1,4 @@
+<?php $array = explode('/', $_SERVER['REQUEST_URI']); $url = array_slice($array, 2); ?>
 <html>
     <head>
         <meta charset="utf-8">
@@ -230,10 +231,18 @@
                     <ul class="sidebar-menu">
                         <li class="header">MENU</li>
                         <!-- Optionally, you can add icons to the links -->
-                        <li class="active"><a href="<?php echo BASE_URL; ?>"><i class="fa fa-home"></i> <span>Home</span></a></li>
-                        <li><a href="<?php echo BASE_URL; ?>/permission"><i class="fa fa-gears"></i> <span>Permissões</span></a></li>
-                        <li><a href="<?php echo BASE_URL; ?>/user"><i class="fa fa-users"></i> <span>Usuários</span></a></li>
-                        <li><a href="<?php echo BASE_URL; ?>/client"><i class="fa fa-street-view"></i> <span>Clientes</span></a></li>
+                        <li class="<?php echo (in_array("",$url))? "active":""; ?>">
+                            <a href="<?php echo BASE_URL; ?>"><i class="fa fa-home"></i> <span>Home</span></a>
+                        </li>
+                        <li class="<?php echo (in_array("permission",$url))? "active":""; ?>">
+                            <a href="<?php echo BASE_URL; ?>/permission"><i class="fa fa-gears"></i> <span>Permissões</span></a>
+                        </li>
+                        <li class="<?php echo (in_array("user",$url))? "active":""; ?>">
+                            <a href="<?php echo BASE_URL; ?>/user"><i class="fa fa-users"></i> <span>Usuários</span></a>
+                        </li>
+                        <li class="<?php echo (in_array("client",$url))? "active":""; ?>">
+                            <a href="<?php echo BASE_URL; ?>/client"><i class="fa fa-street-view"></i> <span>Clientes</span></a>
+                        </li>
                         <li class="treeview">
                             <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
                                 <span class="pull-right-container">
@@ -267,46 +276,14 @@
             <aside class="control-sidebar control-sidebar-dark">
                 <!-- Create the tabs -->
                 <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-                    <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-                    <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+                    <li class="active"><a href="#control-sidebar-permission-tab" data-toggle="tab"><i class="fa fa-gear"></i></a></li>
+                    <li><a href="#control-sidebar-group-permission-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
                 </ul>
                 <!-- Tab panes -->
                 <div class="tab-content">
                     <!-- Home tab content -->
-                    <div class="tab-pane active" id="control-sidebar-home-tab">
-                        <h3 class="control-sidebar-heading">Recent Activity</h3>
-                        <ul class="control-sidebar-menu">
-                            <li>
-                                <a href="javascript:;">
-                                    <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-                                    <div class="menu-info">
-                                        <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
-                                        <p>Will be 23 on April 24th</p>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        <!-- /.control-sidebar-menu -->
-
-                        <h3 class="control-sidebar-heading">Tasks Progress</h3>
-                        <ul class="control-sidebar-menu">
-                            <li>
-                                <a href="javascript:;">
-                                    <h4 class="control-sidebar-subheading">
-                                        Custom Template Design
-                                        <span class="pull-right-container">
-                                            <span class="label label-danger pull-right">70%</span>
-                                        </span>
-                                    </h4>
-
-                                    <div class="progress progress-xxs">
-                                        <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
+                    <div class="tab-pane active" id="control-sidebar-permission-tab">
+                        <h3 class="control-sidebar-heading">Lista de Permissões</h3>
                         <!-- /.control-sidebar-menu -->
 
                     </div>
@@ -315,21 +292,10 @@
                     <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
                     <!-- /.tab-pane -->
                     <!-- Settings tab content -->
-                    <div class="tab-pane" id="control-sidebar-settings-tab">
+                    <div class="tab-pane" id="control-sidebar-group-permission-tab">
                         <form method="post">
-                            <h3 class="control-sidebar-heading">General Settings</h3>
+                            <h3 class="control-sidebar-heading">Grupo de Permissões</h3>
 
-                            <div class="form-group">
-                                <label class="control-sidebar-subheading">
-                                    Report panel usage
-                                    <input type="checkbox" class="pull-right" checked>
-                                </label>
-
-                                <p>
-                                    Some information about this general settings option
-                                </p>
-                            </div>
-                            <!-- /.form-group -->
                         </form>
                     </div>
                     <!-- /.tab-pane -->
