a08855782b50b41f61629f9ed3f4ac6bef1d7d30
igor.muller <igor_muller@hotmail.com>
2017-06-14 15:38:45 -0300

    Ajustado o Core para tratar barras ao final da url

--- core/Core.php
+++ core/Core.php
@@ -9,13 +9,11 @@ class Core{
 
             $url = explode('/', $url);           
             array_shift($url);
-            if ($url[count($url)-1] == ""){
-                array_pop($url);
-            }
+
             $currentController = $url[0].'Controller';
             array_shift($url);
 
-            if (isset($url[0])) {
+            if (isset($url[0]) && !empty($url[0])) {
                     $currentAction = $url[0];
                     array_shift($url);
             } else {
